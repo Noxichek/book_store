7eb0d3264aace453a59c34e6b0ac914b13545e4e
Nikita Dachkovskii <noxik7777@gmail.com>
2022-06-06 19:08:42 +0300

    Create change angular material themes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, HostBinding} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app_book';
+  @HostBinding('class') className = 'defaultMode';
+
+  changeTheme() {
+    const darkClassName = 'darkMode';
+    const defaultName = 'defaultMode'
+
+    this.className = this.className === defaultName ? darkClassName : defaultName
+  }
 }
