ebc9c2608e4270f49dbb3848c0846f36077374da
Nikita Dachkovskii <noxik7777@gmail.com>
2022-06-07 13:20:58 +0300

    Rename selector

--- src/app/layout/components/dashboard-component/dashboard.component.ts
+++ src/app/layout/components/dashboard-component/dashboard.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {Router} from "@angular/router";
 
 @Component({
-  selector: 'app-layout',
+  selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
