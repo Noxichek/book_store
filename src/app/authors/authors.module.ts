5cdab1d9695271f17472d9ba47874272435a8ce6
Nikita Dachkovskii <noxik7777@gmail.com>
2022-06-08 17:07:07 +0300

    Create routing modules for components

--- src/app/authors/authors.module.ts
+++ src/app/authors/authors.module.ts
@@ -2,18 +2,15 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {AuthorsComponent} from "./components/authors-component/authors.component";
 import {MatTableModule} from "@angular/material/table";
-import {RouterModule, Routes} from "@angular/router";
+import {AuthorsRoutingModule} from "./authors-routing.module";
 
-const routes: Routes = [
-  {path: '', component: AuthorsComponent}
-]
 
 @NgModule({
   declarations: [AuthorsComponent],
   imports: [
     CommonModule,
     MatTableModule,
-    RouterModule.forChild(routes)
+    AuthorsRoutingModule
   ],
   exports: [AuthorsComponent]
 })
