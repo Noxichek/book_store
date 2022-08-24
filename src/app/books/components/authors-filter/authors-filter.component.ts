86e101eca6e88dfc2daa34cfce125f318870f15b
Дачковский Никита <noxik7777@gmail.com>
2022-08-24 13:48:27 +0300

    Complete custom mat-form-field

--- src/app/books/components/authors-filter/authors-filter.component.ts
+++ src/app/books/components/authors-filter/authors-filter.component.ts
@@ -1,23 +1,41 @@
-import { Component, HostBinding, Input, OnInit } from '@angular/core';
+import { Component, HostBinding, Input, OnInit, Optional, Self } from '@angular/core';
+import { ControlValueAccessor, FormBuilder, FormControl, NgControl } from '@angular/forms';
+
 import { MatFormFieldControl } from '@angular/material/form-field';
+
+import { Subject, takeUntil } from 'rxjs';
+
 import { IAuthor } from '../../../authors/interfaces/author.interface';
-import { Subject } from 'rxjs';
-import { NgControl } from '@angular/forms';
 
 @Component({
   selector: 'app-authors-filter',
   templateUrl: './authors-filter.component.html',
   styleUrls: ['./authors-filter.component.scss'],
-  providers: [{
-    provide: MatFormFieldControl,
-    useExisting: AuthorsFilterComponent,
-  }],
+  providers: [
+    {
+      provide: MatFormFieldControl,
+      useExisting: AuthorsFilterComponent,
+    },
+  ],
 })
-export class AuthorsFilterComponent implements OnInit, MatFormFieldControl<IAuthor[]> {
+export class AuthorsFilterComponent implements
+  OnInit,
+  ControlValueAccessor,
+  MatFormFieldControl<IAuthor[]> {
 
   private static _nextId = 0;
 
   public stateChanges = new Subject<void>();
+  public errorState = false;
+  public controlType = 'authorFilter';
+  public focused: boolean = false;
+  public authorFilter = new FormControl('');
+
+  @Input()
+  public required!: boolean;
+
+  @Input()
+  public disabled!: boolean;
 
   @HostBinding()
   public id = `authors-filter-id-${AuthorsFilterComponent._nextId++}`;
@@ -33,6 +51,27 @@ export class AuthorsFilterComponent implements OnInit, MatFormFieldControl<IAuth
   }
 
   private _value!: IAuthor[];
+  private _placeholder!: string;
+  private _destroy = new Subject<void>();
+
+  constructor(
+    @Optional() @Self() public ngControl: NgControl,
+    private _formBuilder: FormBuilder,
+  ) {
+    if(this.ngControl !== null) {
+      this.ngControl.valueAccessor = this;
+    }
+  }
+
+  public writeValue(value: IAuthor[]): void {
+    this._value = value;
+  }
+  public registerOnChange(onChange: (value: IAuthor[]) => void): void {
+    this._onChange = onChange;
+  }
+  public registerOnTouched(onTouched: () => void): void {
+    this._touchFn = onTouched;
+  }
 
   @Input()
   public set placeholder(value: string) {
@@ -44,29 +83,42 @@ export class AuthorsFilterComponent implements OnInit, MatFormFieldControl<IAuth
     return this._placeholder;
   }
 
-  private _placeholder!: string;
-
-
-  ngControl: NgControl | null = null;
-  focused: boolean = false;
-  empty!: boolean;
-  shouldLabelFloat: boolean = true;
-  required!: boolean;
-  disabled!: boolean;
-  errorState!: boolean;
-  controlType?: string | undefined;
-  autofilled?: boolean | undefined;
-  userAriaDescribedBy?: string | undefined;
-
-  setDescribedByIds(ids: string[]): void {
-    throw new Error('Method not implemented.');
+  public get empty(): boolean {
+    return false;
   }
 
-  onContainerClick(event: MouseEvent): void {
-    throw new Error('Method not implemented.');
+  @HostBinding('class.floated')
+  public get shouldLabelFloat(): boolean {
+    return true;
   }
 
-  ngOnInit(): void {
+  public setDescribedByIds(ids: string[]): void {
+    // throw new Error('Method not implemented.');
   }
+
+  public onContainerClick(event: MouseEvent): void {
+    // throw new Error('Method not implemented.');
+  }
+
+  public ngOnInit(): void {
+    this.authorFilter.valueChanges
+      .pipe(takeUntil(this._destroy))
+      .subscribe((value) => {
+        console.log(this._value);
+      });
+  }
+
+  public ngOnDestroy(): void {
+    this.stateChanges.next();
+    this.stateChanges.complete();
+    this._destroy.next();
+    this._destroy.complete();
+  }
+
+
+  // eslint-disable-next-line no-empty-function
+  private _onChange = (value: IAuthor[]): void => {};
+  // eslint-disable-next-line no-empty-function
+  private _touchFn = (): void => {};
 
 }
